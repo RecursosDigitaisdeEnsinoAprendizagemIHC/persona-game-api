d4b0123c1e3c0cd924a1779250c916a64d420787
Rossicler Júnior <rossiclerjr@gmail.com>
2021-08-14 19:28:49 -0300

    Added project structure

---
+++ persona-game-api/src/database/index.ts
@@ -0,0 +1,22 @@
+import { createConnection } from "typeorm";
+
+const connection = createConnection({
+  type: "mysql",
+  host: "172.17.0.1",
+  port: parseInt(process.env.MYSQL_TCP_PORT),
+  username: "root",
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  migrations: ["src/database/migrations/*.ts"],
+  entities: ["src/entities/*.ts"],
+  cli: {
+    migrationsDir: "src/database/migrations",
+    entitiesDir: "src/entities",
+  },
+})
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
